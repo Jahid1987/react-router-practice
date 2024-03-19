54b9e6af1054ad391ffe04101de7b4091cdc0726
Jahid Anwer <jahid.englaa@gmail.com>
2024-03-20 00:17:27 +0600

    Home component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Loading from "./Components/Loading";
+import Home from "./Components/Home";
 
 function App() {
   const navigation = useNavigation();
-
+  const location = useLocation();
+  console.log(location);
   return (
     <>
       <nav>
         <Navbar></Navbar>
+        {location.pathname === "/" ? <Home></Home> : ""}
         <main className="mb-60 max-w-[95%] mx-auto">
           {navigation.state === "loading" ? (
             <Loading></Loading>
---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div className="text-center mt-10">
+      <h1 className=" text-3xl lg:text-5xl font-bold">React Router Project </h1>
+      <h2 className=" text-2xl lg:text-4xl font-semibold">Used features: </h2>
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        <li> useLoaderData hook</li>
+        <li>useNavigation hook</li>
+        <li>useNavigate Hook</li>
+        <li>And so on</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
